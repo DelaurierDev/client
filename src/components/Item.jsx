9e3c61ba1113126e7edc96dc9165231198eb70dc
DelaurierDev <delaurier4@gmail.com>
2023-11-22 23:19:56 -0700

    starting the item card

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Item = (props) => {
   return (
-    <div>Item</div>
+    <div>
+      <p>This is the start of an item Card.  I am going to be an item card for an ecommerce store at some poing.</p>
+    </div>
   )
 }
 
