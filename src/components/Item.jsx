5369a2eea136f7834a3dbc1b455bef1fd396319c
DelaurierDev <delaurier4@gmail.com>
2023-11-24 20:37:57 -0700

    building out the item component more

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Item = (props) => {
   return (
     <div>
-      <p>This is the start of an item Card.  I am going to be an item card for an ecommerce store at some poing.</p>
+      <p>{props.title}</p>
+      <p>${props.price}</p>
+      <p>{props.description}</p>
+      <button className='text-[#1d4aa8] group border-2 border-[#1d4aa8] px-6 py-3 my-2 flex items-center hover:bg-[#1d4aa8] hover:border-black hover:text-white'>Add to card</button>
     </div>
   )
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,7 +5,7 @@ const Shop = () => {
   return (
     <div name="shop" className='my-[200px] max-w-[1000px] px-7 mx-auto'>
         <p className='text-7xl border-b-2 w-[180px] align-middle'>Shop</p>
-        <Item/>
+        <Item title = "Cool bag" price = "23.99" description = "super cool bag"/>
     </div>
   )
 }
