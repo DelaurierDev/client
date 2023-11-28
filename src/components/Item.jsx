2e4e7e2443517e342ed53af04e569b9ac1a4bffe
DelaurierDev <delaurier4@gmail.com>
2023-11-27 22:03:54 -0700

    styling the item card and working on shop componen

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Item = (props) => {
   return (
-    <div>
-      <p>{props.title}</p>
+    <div className='border w-[300px] flex flex-col items-center p-5 my-5 shadow-lg hover:bg-gray-50'>
+      <p className='text-xl'>{props.title}</p>
       <p>${props.price}</p>
-      <p>{props.description}</p>
+      <p className='text-l'>{props.description}</p>
       <button className='text-[#1d4aa8] group border-2 border-[#1d4aa8] px-6 py-3 my-2 flex items-center hover:bg-[#1d4aa8] hover:border-black hover:text-white'>Add to card</button>
     </div>
   )
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,9 +3,19 @@ import Item from '../components/Item'
 
 const Shop = () => {
   return (
+    
     <div name="shop" className='my-[200px] max-w-[1000px] px-7 mx-auto'>
+
         <p className='text-7xl border-b-2 w-[180px] align-middle'>Shop</p>
-        <Item title = "Cool bag" price = "23.99" description = "super cool bag"/>
+
+        <div className='flex justify-around'>
+          <Item title = "Cool bag" price = "23.99" description = ""/>
+          <Item title = "Cool shoes" price = "28.99" description = ""/>
+          <Item title = "Cool dog" price = "0.99" description = ""/>          
+        </div>
+
+  
+
     </div>
   )
 }
