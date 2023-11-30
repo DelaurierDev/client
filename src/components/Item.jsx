1350822f160fc86e14b60d55198dcefe25b3f033
DelaurierDev <delaurier4@gmail.com>
2023-11-29 18:03:13 -0700

    adding more styling and responsivness to shop

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Item = (props) => {
   return (
-    <div className='border w-[300px] flex flex-col items-center p-5 my-5 shadow-lg hover:bg-gray-50'>
+    <div className='border w-full flex flex-col items-center p-5 m shadow-lg hover:bg-gray-50'>
       <p className='text-xl'>{props.title}</p>
       <p>${props.price}</p>
-      <p className='text-l'>{props.description}</p>
       <button className='text-[#1d4aa8] group border-2 border-[#1d4aa8] px-6 py-3 my-2 flex items-center hover:bg-[#1d4aa8] hover:border-black hover:text-white'>Add to card</button>
     </div>
   )
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -8,24 +8,13 @@ const Shop = () => {
 
         <p className='text-7xl border-b-2 w-[180px] align-middle'>Shop</p>
 
-        <div className='flex space-around'>
-          <div >
+        <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8'>
             <Item title = "Cool bag" price = "23.99" description = ""/>
             <Item title = "Cool shoes" price = "28.99" description = ""/>
-
-          </div>
-          <div>
             <Item title = "Cool bag" price = "23.99" description = ""/>
             <Item title = "Cool shoes" price = "28.99" description = ""/>
-
-          </div>
-          <div>
             <Item title = "Cool bag" price = "23.99" description = ""/>
             <Item title = "Cool shoes" price = "28.99" description = ""/>
-
-          </div>
-         
-         
         </div>
 
   
