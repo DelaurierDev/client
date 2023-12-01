0445ec36b9e6dbda1f005f7432e54b6b38810e15
DelaurierDev <delaurier4@gmail.com>
2023-11-30 22:22:58 -0700

    adding actual shop items in the shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Item from '../components/Item'
+import Nike from '../assets/nike.png'
+import PullUp from '../assets/pull up.jpg'
 
 const Shop = () => {
   return (
@@ -9,12 +11,12 @@ const Shop = () => {
         <p className='text-7xl border-b-2 w-[180px] align-middle'>Shop</p>
 
         <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8'>
-            <Item title = "Cool bag" price = "23.99" description = ""/>
-            <Item title = "Cool shoes" price = "28.99" description = ""/>
-            <Item title = "Cool bag" price = "23.99" description = ""/>
-            <Item title = "Cool shoes" price = "28.99" description = ""/>
-            <Item title = "Cool bag" price = "23.99" description = ""/>
-            <Item title = "Cool shoes" price = "28.99" description = ""/>
+            <Item img = {PullUp} title = "Pull up bar" price = "27.99" description = ""/>
+            <Item img = "" title = "Resistence Bands" price = "59.99" description = ""/>
+            <Item img = "" title = "Gymshark Gym Bag" price = "28.80" description = ""/>
+            <Item img = {Nike} title = "Nike Running Shoes" price = "64.97" description = ""/>
+            <Item img = "" title = "Multi-Purpose Treadmill" price = "349.99" description = ""/>
+            <Item img = "" title = "All-in-One Suspension Training System" price = "96.99" description = ""/>
         </div>
 
   
