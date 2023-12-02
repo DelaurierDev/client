20fbb7a61c8480c0023fbb2fabf4f138ee5d6398
DelaurierDev <delaurier4@gmail.com>
2023-12-01 21:54:12 -0700

    finishing styling of shop component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Item from '../components/Item'
 import Nike from '../assets/nike.png'
-import PullUp from '../assets/pull up.jpg'
+import PullUp from '../assets/pullup.png'
+import Bands from '../assets/bands.jpg'
+import Bag from '../assets/gymshark.jpg'
+import Mill from '../assets/treadmill.jpg'
+import Susp from '../assets/suspension.jpg'
 
 const Shop = () => {
   return (
@@ -11,12 +15,12 @@ const Shop = () => {
         <p className='text-7xl border-b-2 w-[180px] align-middle'>Shop</p>
 
         <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center py-8'>
-            <Item img = {PullUp} title = "Pull up bar" price = "27.99" description = ""/>
-            <Item img = "" title = "Resistence Bands" price = "59.99" description = ""/>
-            <Item img = "" title = "Gymshark Gym Bag" price = "28.80" description = ""/>
+            <Item img = {PullUp} title = "Pull up bar" price = "199.99" description = ""/>
+            <Item img = {Bands} title = "Resistence Bands" price = "59.99" description = ""/>
+            <Item img = {Bag} title = "Gymshark Gym Bag" price = "28.80" description = ""/>
             <Item img = {Nike} title = "Nike Running Shoes" price = "64.97" description = ""/>
-            <Item img = "" title = "Multi-Purpose Treadmill" price = "349.99" description = ""/>
-            <Item img = "" title = "All-in-One Suspension Training System" price = "96.99" description = ""/>
+            <Item img = {Mill} title = "Multi-Purpose Treadmill" price = "349.99" description = ""/>
+            <Item img = {Susp} title = "All-in-One Suspension Training System" price = "96.99" description = ""/>
         </div>
 
   
